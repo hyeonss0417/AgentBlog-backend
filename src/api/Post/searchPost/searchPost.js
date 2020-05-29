192230bc677c9333585126b8a88c0bac2a1bdb9f
전현성 <skyoun97@gmail.com>
2020-05-30 01:09:17 +0900

    Upgrade - searchPost perfomance

--- src/api/Post/searchPost/searchPost.js
+++ src/api/Post/searchPost/searchPost.js
@@ -6,9 +6,14 @@ export default {
       if (args.term === "") {
         return null;
       }
+      const term = args.term.toLowerCase();
       return await prisma.posts({
         where: {
-          OR: [{title_contains: args.term}, {content_contains: args.term}],
+          OR: [
+            {title_contains: term},
+            {content_contains: term},
+            {hashtags_some: {name_contains: term}},
+          ],
         },
       });
     },
