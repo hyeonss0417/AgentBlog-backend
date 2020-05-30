adc5ac5fd7fad5c07728b894654fdaa1dc1f1b44
전현성 <skyoun97@gmail.com>
2020-05-30 23:25:14 +0900

    Done - URL uniqueness & getPostDetail Query

--- src/GetUniqueUrl.js
+++ src/GetUniqueUrl.js
@@ -1,17 +1,17 @@
-import { generateSecret } from "./utils";
+import {generateSecret} from "./utils";
 
-export const GetUniqueUrl = (username, url) => {
-    let uniqueUrl = url;
+export const GetUniqueUrl = async (username, url) => {
+  let uniqueUrl = url;
 
-    let existingPost = true;
-    while (existingPost) {
-      existingPost = await prisma.$exists.post({
-        user: {username},
-        url:uniqueUrl,
-      });
-      if (existingPost) {
-        uniqueUrl = url + generateSecret();
-      }
+  let existingPost = true;
+  while (existingPost) {
+    existingPost = await prisma.$exists.post({
+      user: {username},
+      url: uniqueUrl,
+    });
+    if (existingPost) {
+      uniqueUrl = url + generateSecret();
     }
-    return uniqueUrl;
-}
+  }
+  return uniqueUrl;
+};
