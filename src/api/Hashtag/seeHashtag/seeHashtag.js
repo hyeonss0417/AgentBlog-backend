165bde6b0bc817d29f2dc261699bc6cc6ad7e2e2
전현성 <skyoun97@gmail.com>
2020-06-06 02:20:10 +0900

    Fix - seeHashtag to show explaination field

--- src/api/Hashtag/seeHashtag/seeHashtag.js
+++ src/api/Hashtag/seeHashtag/seeHashtag.js
@@ -3,7 +3,7 @@ import {prisma} from "../../../../generated/prisma-client";
 export default {
   Query: {
     searchHashtag: (_, args) => {
-      return prisma.hashtag({name: args.name}).posts();
+      return prisma.hashtag({name: args.name});
     },
   },
 };
