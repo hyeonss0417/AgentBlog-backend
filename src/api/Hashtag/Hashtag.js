163e6b9cbf8e357fc60987c614c7fbbeee82b39e
전현성 <skyoun97@gmail.com>
2020-06-06 02:42:25 +0900

    Bug Fix - Hashtag

--- src/api/Hashtag/Hashtag.js
+++ src/api/Hashtag/Hashtag.js
@@ -3,7 +3,7 @@ import {prisma} from "../../../generated/prisma-client";
 export default {
   Hashtag: {
     posts: (parent, _) => {
-      return prisma.Hashtag({id: parent.id}).posts();
+      return prisma.hashtag({id: parent.id}).posts();
     },
   },
 };
