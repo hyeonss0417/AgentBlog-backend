478a4c651173f80cc29a591c81a3f8e00ccd66a1
전현성 <skyoun97@gmail.com>
2020-06-07 02:36:16 +0900

    Done - File upload to S3 server

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import schema from "./schema";
 import "./passport";
 import {authenticateJwt} from "./passport";
 import {checkIfAuthenticated} from "./middleware";
+import {uploadMiddleware, uploadController} from "./upload";
 
 const PORT = process.env.PORT || 4000;
 const typeDefs = `
@@ -24,6 +25,7 @@ const server = new GraphQLServer({
 
 server.express.use(logger("dev"));
 server.express.use(authenticateJwt);
+server.express.post("/api/upload", uploadMiddleware, uploadController);
 
 server.start({port: PORT}, () =>
   console.log(`✅ Server running on http://localhost:${PORT}`)
---
+++ src/upload.js
@@ -0,0 +1,29 @@
+import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: "ap-northeast-2",
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: "agent-blog",
+    metadata: function(req, file, cb) {
+      cb(null, {fieldName: file.fieldname});
+    },
+    key: function(req, file, cb) {
+      cb(null, Date.now().toString());
+    },
+  }),
+});
+export const uploadMiddleware = upload.single("file");
+
+export const uploadController = (req, res) => {
+  const location = req.file.location;
+  res.json({location});
+  res.end();
+};
