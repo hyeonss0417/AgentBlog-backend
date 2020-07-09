ebae9d2a9aa6f18920d4c6ef0d980e9dd75ea0a4
전현성 <skyoun97@gmail.com>
2020-07-10 03:38:41 +0900

    [Doing] - corsOptions

--- src/server.js
+++ src/server.js
@@ -15,9 +15,15 @@ const server = new GraphQLServer({
   context: ({request}) => ({request, checkIfAuthenticated}),
 });
 
+const corsOptions = {
+  origin: "uri of front-end",
+  credentials: true,
+};
+
 server.express.use(logger("dev"));
 server.express.use(authenticateJwt);
 server.express.use(cors());
+//server.express.use(cors({corsOptions}));
 server.express.post("/api/upload", uploadController);
 
 server.start({port: PORT}, () =>
