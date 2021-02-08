5ab0edb1fe6355b0c52b84ded6c52bd1a57f5201
전현성 <skyoun97@gmail.com>
2021-02-09 02:00:12 +0900

    Setting - CORS

--- src/server.js
+++ src/server.js
@@ -16,14 +16,39 @@ const server = new GraphQLServer({
   context: ({request, response}) => ({request, response, checkIfAuthenticated}),
 });
 
-const corsOptions = {
-  origin: "http://52.78.67.10",
-  credentials: true,
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     const whiteList = ["https://agent-blog-frontend.herokuapp.com", "http://52.78.67.10"];
+//     whiteList.indexOf
+//     db.loadOrigins(function (error, origins) {
+//       callback(error, origins)
+//     })
+//   },
+//   credentials: true,
+// };
+
+var customCors = function(req, res, next) {
+  var whitelist = [
+    "https://agent-blog-frontend.herokuapp.com",
+    "http://52.78.67.10",
+  ];
+  var origin = req.headers.origin;
+  if (whitelist.indexOf(origin) > -1) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type,Authorization,X-Requested-With"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
 };
 
 server.express.use(logger("dev"));
 server.express.use(authenticateJwt);
-server.express.use(cors(corsOptions));
+//server.express.use(cors(corsOptions));
+server.express.use(customCors);
 server.express.use(cookieParser());
 server.express.post("/api/upload", uploadController);
 
